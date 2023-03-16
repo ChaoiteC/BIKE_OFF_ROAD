6152bc6ae415aef2b569a69c37672539885de579
邱润 <12142665+RRRRRRRun@user.noreply.gitee.com>
2023-03-16 12:24:24 +0000

    update project/code/imath.h. 对函数u32RangeLimit做出声明
    Signed-off-by: 邱润 <12142665+RRRRRRRun@user.noreply.gitee.com>

--- project/code/imath.h
+++ project/code/imath.h
@@ -9,6 +9,7 @@ void  setFloatValue(SI_F_XYZ *_in_data,float value);
 float f_abs(float f);
 uint16_t u16RangeLimit(uint16_t thr_in,uint16_t thr_min,uint16_t thr_max);
 int int_abs(int f);
+uint32 u32RangeLimit(uint32 thr_in,uint32 thr_min,uint32 thr_max);
 
 
 #endif
